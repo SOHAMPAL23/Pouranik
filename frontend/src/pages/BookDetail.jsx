13625163c5b8bf97db9f2ad6eb05a0a63b8b8701
SOHAMPAL23 <palsoham074@gmail.com>
2025-07-24 17:51:49 +0530

    bookDetails

--- frontend/src/pages/BookDetail.jsx
+++ frontend/src/pages/BookDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-
+import AISummarySection from './AISummarySection';
 
 export default function BookDetail() {
   const { id } = useParams();
